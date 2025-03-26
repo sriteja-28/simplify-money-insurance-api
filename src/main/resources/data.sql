f75c11ed4dedcf0c367cbc683b7cb827c49999cd
MUTHANGI SRITEJA <108087209+sriteja-28@users.noreply.github.com>
2025-03-26 22:41:06 +0530

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+-- Insert sample data into INSURANCE table
+INSERT INTO INSURANCE (name, type, price) VALUES ('Health Insurance', 'Health', 5000);
+INSERT INTO INSURANCE (name, type, price) VALUES ('Life Insurance', 'Life', 7000);
